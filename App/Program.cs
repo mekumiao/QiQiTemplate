de400b66a63fca73668c6b81c959bb38357df75e
qiqigou <2319002545@qq.com>
2020-07-03 23:54:54 +0800

    update

--- App/Program.cs
+++ App/Program.cs
@@ -7,15 +7,15 @@ namespace App
     {
         static void Main(string[] args)
         {
-            var node = new ELSEIFNodeContext("{{ idx >= 2}}", null);
+            //var node = new ELSEIFNodeContext("{{ idx >= 2}}", null);
 
             var provide = new NodeContextProvide();
 
-            var lambda = provide.BuildTemplateByPath(@"Test\Temp.txt");
+            var lambda = provide.BuildTemplateByPath(@"Temp.txt");
             var action = lambda.Compile();
 
             var model = new FieldDynamicModel();
-            model.LoadByPath(@"Test\Temp.json");
+            model.LoadByPath(@"Temp.json");
 
             action.Invoke(model);
         }
